83093b5a0bc9fd724c3a13f1620b4bb1b3a33cff
a3-1811 <huydevmods1811@gmail.com>
2022-02-22 15:02:08 +0700

    config tailwind CSS file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      primary: '#1d90f4',
+      secondary: '#63b7b1',
+    },
     extend: {},
   },
   plugins: [],
