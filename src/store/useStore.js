ba091468e9d1c966d0cf2b99baf41da24c24960c
a3-1811 <huydevmods1811@gmail.com>
2022-02-22 17:32:06 +0700

    Setup Zutand for management state app

---
+++ src/store/useStore.js
@@ -0,0 +1,9 @@
+import create from "zustand";
+
+import createAuthSlice from "./createAuthSlice";
+
+const useStore = create((set, get) => ({
+  ...createAuthSlice(set, get),
+}));
+
+export default useStore;
