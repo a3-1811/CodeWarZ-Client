9fcd1b438df365e30a64a2ae1934a410136cf1d0
Nam Duong <duongquocnam224400@gmail.com>
2022-02-23 13:45:53 +0700

    Updated-ForgotPasswordPage

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,34 @@
-import Login from "./pages/page/Login";
-import Register from "./pages/page/Register";
-import Main from "./pages/page/Main";
-import Profile from "./pages/page/Profile";
-import { Navigate } from "react-router-dom";
-import Page404 from "./pages/page404";
+import Login from './pages/page/Login';
+import Register from './pages/page/Register';
+import Main from './pages/page/Main';
+import Profile from './pages/page/Profile';
+import { Navigate } from 'react-router-dom';
+import Page404 from './pages/page404';
+import ForgotPass from './pages/page/ForgotPass';
 
-const routes = (isLoggedIn) => [
+const routes = isLoggedIn => [
   {
-    path: "/",
+    path: '/',
     element: isLoggedIn ? <Main /> : <Navigate to="/login" />,
   },
-  { path: "/profile", element:  isLoggedIn ? <Profile /> : <Navigate to="/login"/> },
   {
-    path: "/login",
+    path: '/profile',
+    element: isLoggedIn ? <Profile /> : <Navigate to="/login" />,
+  },
+  {
+    path: '/login',
     element: !isLoggedIn ? <Login /> : <Navigate to="/" />,
   },
-  { path: "/register", element: !isLoggedIn ? <Register /> : <Navigate to="/"/>},
   {
-    path: "*",
+    path: '/register',
+    element: !isLoggedIn ? <Register /> : <Navigate to="/" />,
+  },
+  {
+    path: '/forgot-password',
+    element: !isLoggedIn ? <ForgotPass /> : <Navigate to="/" />,
+  },
+  {
+    path: '*',
     element: <Page404 />,
   },
 ];
